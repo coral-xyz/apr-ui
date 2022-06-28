b72bbf5726be6e461a478302896eac22b1dac877
anoushk1234 <anoushka1828@gmail.com>
2022-06-28 12:07:12 +0530

    feature: visual feedback after name update

--- pages/account.tsx
+++ pages/account.tsx
@@ -1,11 +1,21 @@
 import { useEffect, useState } from "react";
 import Layout from "../components/layout";
-import { Alert, AlertTitle, Box, Grid, TextField } from "@mui/material";
+import {
+  Alert,
+  AlertTitle,
+  Box,
+  Grid,
+  Slide,
+  SlideProps,
+  Snackbar,
+  TextField,
+} from "@mui/material";
 import fetch from "isomorphic-unfetch";
 import useAuth from "../hooks/useAuth";
 import fetcher from "../utils/fetcher";
 import useSWR from "swr";
 import Tokens from "../components/account/tokens";
+import { TransitionProps } from "@mui/material/transitions";
 
 const metaTags = {
   title: "apr",
@@ -13,27 +23,71 @@ const metaTags = {
   shouldIndex: false,
   url: "https://apr.dev",
 };
-
+function SlideTransition(props: SlideProps) {
+  return <Slide {...props} direction="up" />;
+}
 export default function Account() {
   const { session, status } = useAuth(true);
   const { data: user } = useSWR("/api/user", fetcher);
   const [username, setUsername] = useState("");
-
-  useEffect(() => {
-    if (username === "" && user) setUsername(user.username);
-  }, [username, user]);
+  const [open, setOpen] = useState<{
+    open: boolean;
+    Transition: React.ComponentType<
+      TransitionProps & {
+        children: React.ReactElement<any, any>;
+      }
+    >;
+  }>({
+    open: false,
+    Transition: Slide,
+  });
+  // useEffect(() => {
+  //   if (username === "" && user) setUsername(user.username);
+  // }, [username, user]);
 
   if (!session) return null;
+  const handleClick =
+    (
+      Transition: React.ComponentType<
+        TransitionProps & {
+          children: React.ReactElement<any, any>;
+        }
+      >
+    ) =>
+    () => {
+      setOpen({
+        open: true,
+        Transition,
+      });
+    };
 
+  const handleClose = () => {
+    setOpen({
+      ...open,
+      open: false,
+    });
+  };
   const handleUpdateUsername = async () => {
     await fetch(`/api/user`, {
       method: "PUT",
       body: JSON.stringify({ username }),
     });
+    handleClick(SlideTransition)();
   };
 
   return (
     <Layout metaTags={metaTags}>
+      <Snackbar
+        open={open.open}
+        autoHideDuration={3000}
+        onClose={handleClose}
+        TransitionComponent={open.Transition}
+        key={open.Transition.name}
+      >
+        <Alert severity="success" sx={{ width: "100%" }}>
+          User name updated!
+        </Alert>
+      </Snackbar>
       <Grid container gap={10}>
         <Grid item xs={12} sx={{ display: "flex", justifyContent: "center" }}>
           <Box
