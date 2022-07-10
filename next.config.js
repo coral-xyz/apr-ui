23886ba38072088ac26adc52d763dcbc8e0c67a4
Italo Casas <me@italoacasas.com>
2022-07-10 09:05:55 +0200

    adding sentry integration

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
+const { withSentryConfig } = require("@sentry/nextjs");
 const { withPlausibleProxy } = require("next-plausible");
 
-/** @type {import('next').NextConfig} */
-module.exports = withPlausibleProxy()({
+const moduleExports = withPlausibleProxy()({
   experimental: {
     newNextLinkBehavior: true,
     scrollRestoration: true,
@@ -18,3 +18,19 @@ module.exports = withPlausibleProxy()({
     formats: ["image/avif", "image/webp"],
   },
 });
+
+const sentryWebpackPluginOptions = {
+  // Additional config options for the Sentry Webpack plugin. Keep in mind that
+  // the following options are set automatically, and overriding them is not
+  // recommended:
+  //   release, url, org, project, authToken, configFile, stripPrefix,
+  //   urlPrefix, include, ignore
+
+  silent: true, // Suppresses all logs
+  // For all available options, see:
+  // https://github.com/getsentry/sentry-webpack-plugin#options.
+};
+
+// Make sure adding Sentry options is the last code to run before exporting, to
+// ensure that your source maps include changes from all other Webpack plugins
+module.exports = withSentryConfig(moduleExports, sentryWebpackPluginOptions);
